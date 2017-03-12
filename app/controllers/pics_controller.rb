03f00ca38a0129fbba9a2dbba6170501a93f08ae
shhhRakesh <sharma26.riti@gmail.com>
2017-03-13 05:12:28 +0530

    Pics Views Controller and Model Added

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,38 @@
 class PicsController < ApplicationController
-
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
   def index
-    
+    @pics= Pic.all
+  end
+  def show
+    @pic= Pic.find(params[:id])
+  end
+  def new
+    @pic= Pic.new
+  end
+  def create
+    @pic= Pic.new(pic_params)
+    if @pic.save
+      redirect_to @pic, notice: "Yeah HOOKED!!!!"
+    else
+      render 'new'
+    end
+  end
+  def edit
+
+  end
+  def update
+
+  end
+  def destroy
+
+  end
+
+  private
+
+  def pic_params
+    params.require(:pic).permit(:title, :description)
+  end
+  def find_pic
+    @pic= Pic.find(params[:id])
   end
 end
